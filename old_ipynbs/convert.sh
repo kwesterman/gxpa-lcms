4775ba06f5f36cd1b8aa1775cf30a548af83dfbb
phanson2k <phanson@broadinstitute.org>
2024-09-23 18:50:41 +0000

    reorganize, make ignored old/ folder

--- old_ipynbs/convert.sh
+++
@@ -1,3 +0,0 @@
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --to=markdown *.ipynb
-sed -i'' 's/```R/```{r}/' *.md
-for file in *.md; do mv "$file" "${file%.md}.Rmd"; done
