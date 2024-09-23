cf6612ef508a6297648cc64df1ccb69de7bdd9ec
phanson2k <phanson@broadinstitute.org>
2024-09-23 19:26:43 +0000

    include .git in backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 if [ -z "$1" ]; then
   echo "Provide a backup date/name like 240919"
 else
-  gsutil -m cp -r *.Rmd $WORKSPACE_BUCKET/phanson_backup/$1
+  gsutil -m cp -r *.Rmd .git $WORKSPACE_BUCKET/phanson_backup/$1
 fi
