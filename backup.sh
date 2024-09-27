58d06c436cd3957f36fee58ec8dc4f7560624dfc
phanson2k <phanson@broadinstitute.org>
2024-09-27 11:48:28 -0400

    Adjustemnts to work locally instead of in Terra

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 if [ -z "$1" ]; then
   echo "Provide a backup date/name like 240919"
 else
-  gsutil -m cp -r *.Rmd .git $WORKSPACE_BUCKET/phanson_backup/$1
+  gsutil -m cp -r *.Rmd *.sh .git .gitignore gs://fc-secure-4a392455-5587-4d6f-b8bd-01a1f834ae63/phanson_backup/$1
 fi
