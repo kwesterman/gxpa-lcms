6b1dbdf86c6638225723e54ad41af5b769fb702f
phanson2k <phanson@broadinstitute.org>
2024-10-22 12:59:25 -0400

    reorganization
    Renamed 02_analysis_prep.Rmd to pheno preproc since that's mostly what it is right now, made directories for met & pheno intermediate outputs, moved some scripts into old/ folder.

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,1 @@
-rm -rf bcftools htslib an_MESA* MESA_pilot* regions.txt rsids.txt tmp.txt freeze.9b.chr* phenotypes Rplots*
+rm -rf bcftools htslib an_MESA* MESA_pilot* regions.txt rsids.txt tmp.txt freeze.9b.chr* phenotypes metabolomics Rplots*
