b65a3a6cf3fe7d91b5e744c9a2a633dc3ddab4a0
phanson2k <phanson@broadinstitute.org>
2025-04-08 12:44:18 -0400

    Style changes, render tables of results

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,4 @@
-rm -rf *.png *.html *_files *_cache regions.txt rsids.txt
+rm -rf *.png *.html *_files *_cache
 if [ "$1" == "full" ]; then
   rm -rf bcftools htslib phenotypes metabolomics analysis_df.csv
 fi
