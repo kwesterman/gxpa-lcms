7be0a987313083d6d44b16ba735431fe32b3fb5c
phanson2k <phanson@broadinstitute.org>
2026-01-15 15:54:53 -0500

    Add FHS analysis
    But it failed because our variant is AFR-specific and FHS is primarily EUR.

--- push_to_terra.sh
+++ push_to_terra.sh
@@ -2,6 +2,6 @@ git clone https://github.com/kwesterman/gxpa-lcms.git
 cd gxpa-lcms
 gcloud storage cp *.Rmd   gs://fc-secure-4a392455-5587-4d6f-b8bd-01a1f834ae63/notebooks/
 gcloud storage cp *.ipynb gs://fc-secure-4a392455-5587-4d6f-b8bd-01a1f834ae63/notebooks/
-gcloud storage cp variants_of_interest.csv mesa_geno_files_drs.csv gs://fc-secure-4a392455-5587-4d6f-b8bd-01a1f834ae63/
+gcloud storage cp variants_of_interest.csv geno_files_drs.csv gs://fc-secure-4a392455-5587-4d6f-b8bd-01a1f834ae63/
 cd ..
 rm -rf gxpa-lcms
